1a6208ee0890f582c111ac4d1f6da5799ae7e0c0
arman.hakobyan <arman.hakobyan@hawex.io>
2023-04-11 18:19:06 +0300

    NFCS SPACE

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: [process.env.CLIENT_URL, 'https://dev.nfcsportal.com', '*', 'http://localhost:3000', 'https://nfcs-f.onrender.com', 'https://www.nfcs.space', 'https://nfcs.space', 'nfcs.space']
+}));
 app.use('/api', router);
 app.use(errorMiddleware);
 
